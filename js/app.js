c22627976634b83642ec6d8b74697904f2170152
maritza1013 <maritzamae13@gmail.com>
2023-03-28 15:43:23 -0500

    aplicando clases de JavaScript y modificando algunos estilos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('listado de personas')
+// console.log('listado de personas')
+const personas = [
+    new Persona('Juan','Perez'),
+    new Persona('Maria','Rivera'),
+    new Persona('Kevin','Prada')
+]
+
+function mostrarPersonas (){
+    console.log('mostrar personas.....')
+
+    let texto = ''
+    for(persona of personas){
+        console.log(persona)
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`
+    }
+    document.getElementById('personas').innerHTML = texto
+
+}
+
